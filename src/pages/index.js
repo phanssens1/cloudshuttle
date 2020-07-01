b1d856d7b4312cb445edc1c889b3c6a209415da3
Peter Hanssens <peter@MacBook-Pro.lan>
2020-07-01 16:59:02 +1000

    updating seo

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,21 +2,21 @@ import React from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import catAndHumanIllustration from "../images/cloud_shuttle_logo_awesome.png";
+import logoCloudShuttle from "../images/cloud_shuttle_logo_awesome.png";
 
 function IndexPage() {
   return (
     <Layout>
       <SEO
-        keywords={[`gatsby`, `tailwind`, `react`, `tailwindcss`]}
-        title="Home"
+        keywords={[`Cloud Shuttle`, `Peter Hanssens`, `Community Consulting`, `Serverless`, `Data Engineering`]}
+        title="Home - Cloud Shuttle - transporting you to the cloud!"
       />
 
       <section className="text-center">
         <img
           alt="Cat and human sitting on a couch"
           className="block w-1/2 mx-auto mb-8"
-          src={catAndHumanIllustration}
+          src={logoCloudShuttle}
         />
 
         <h2 className="inline-block p-3 mb-4 text-xl ">
